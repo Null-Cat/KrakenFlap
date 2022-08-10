7baa857ae2cb1c77640eee9962e48754a38a224c
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2022-08-10 20:52:03 +0100

    Added gzip Compression on Web Server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const app = express();
+const compression = require('compression')
+const express = require('express')
+const app = express()
 
 const port = process.env.PORT || 8000
 
-app.use(express.static("public"))
+app.use(compression())
+app.use(express.static('public'))
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`)
 })
